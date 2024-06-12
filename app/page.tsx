12caf2029e514c1069260669b9f7b5d426f718b8
louis-md <louis@margot-duclot.fr>
2024-06-12 18:03:15 +0200

    Add links

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,19 @@
 
 import { useState } from 'react'
 import { Safe4337Pack } from '@safe-global/relay-kit'
+import Img from 'next/image'
 
 import PasskeyList from '../components/PasskeyList'
 import { executeUSDCTransfer } from '../lib/usdc'
 import { getPasskeyFromRawId, type PasskeyArgType } from '../lib/passkeys'
-import { BUNDLER_URL, RPC_URL } from '../lib/constants'
+import { BUNDLER_URL, CHAIN_NAME, RPC_URL } from '../lib/constants'
 import { bufferToString } from '../lib/utils'
 
 function Create4337SafeAccount () {
   const [selectedPasskey, setSelectedPasskey] = useState<PasskeyArgType>()
   const [safeAddress, setSafeAddress] = useState<string>()
   const [isSafeDeployed, setIsSafeDeployed] = useState<boolean>()
+  const [userOp, setUserOp] = useState<string>()
 
   const selectPasskeySigner = async (rawId: string) => {
     console.log('selected passkey signer: ', rawId)
@@ -70,16 +72,76 @@ function Create4337SafeAccount () {
           <div style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
             Address: {safeAddress}
           </div>
-          <div>Is deployed?: {isSafeDeployed ? 'Yes' : 'No'}</div>
-
+          <div>
+            Is deployed?:{' '}
+            {isSafeDeployed ? (
+              <a
+                href={`https://app.safe.global/home?safe=sep:${safeAddress}`}
+                target='_blank'
+                rel='noreferrer'
+              >
+                Yes{' '}
+                <Img
+                  src='/external-link.svg'
+                  alt='External link'
+                  width={14}
+                  height={14}
+                />
+              </a>
+            ) : (
+              'No'
+            )}
+          </div>
+          <div>
+            {' '}
+            <a
+              href='https://faucet.circle.com/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Get some test USDC for your Safe{' '}
+              <Img
+                src='/external-link.svg'
+                alt='External link'
+                width={14}
+                height={14}
+              />
+            </a>
+          </div>
           {selectedPasskey && (
             <button
-              onClick={() =>
-                executeUSDCTransfer({ signer: selectedPasskey, safeAddress })
+              onClick={async () =>
+                await executeUSDCTransfer({
+                  signer: selectedPasskey,
+                  safeAddress
+                }).then(userOpHash => {
+                  setUserOp(userOpHash)
+                  setIsSafeDeployed(true)
+                })
               }
             >
               Sign transaction with passkey
             </button>
+          )}
+          {userOp && isSafeDeployed && (
+            <>
+              <div>
+                Done! Check the transaction status on{' '}
+                <a
+                  href={`https://jiffyscan.xyz/userOpHash/${userOp}?network=${CHAIN_NAME}`}
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  Jiffy Scan{' '}
+                  <Img
+                    src='/external-link.svg'
+                    alt='External link'
+                    width={14}
+                    height={14}
+                  />
+                </a>
+              </div>
+            </>
           )}
         </div>
       )}
